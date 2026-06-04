9fd6207b300a16cae99f7b1b1ad5ead9a3f17b32
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-04 16:14:16 +0900

    Align inquiry flow test naming

--- tests/Http/FlowCustomerInquiryTest.php
+++ tests/Http/FlowCustomerInquiryTest.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace MyVendor\BeMart\Tests\Http;
 
 use BEAR\Resource\ResourceInterface;
-use MyVendor\BeMart\Tests\Hypermedia\CustomerInquiryWorkflowTest as Workflow;
+use MyVendor\BeMart\Tests\Hypermedia\FlowCustomerInquiryTest as Workflow;
 
-final class CustomerInquiryWorkflowTest extends Workflow
+final class FlowCustomerInquiryTest extends Workflow
 {
     protected function newResource(): ResourceInterface
     {
         return new HttpResource(
             '127.0.0.1:8081',
             __DIR__ . '/json-index.php',
-            __DIR__ . '/log/customer-inquiry-workflow.log',
+            __DIR__ . '/log/flow-customer-inquiry.log',
         );
     }
 }
