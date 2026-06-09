48f89e0a26c87bb2986eb22b6fc8c8f3e2466a35
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-09 09:22:25 +0900

    Add MediaQuery SQL smoke contract

--- var/sql/tclass_name_put.sql
+++ var/sql/tclass_name_put.sql
@@ -1,4 +1,4 @@
 INSERT INTO dtb_class_name (id, name, sort_no, create_date, update_date, discriminator_type)
-SELECT CAST(JSON_VALUE(:className, '$.classNameId') AS UNSIGNED), JSON_VALUE(:className, '$.name'), COALESCE((SELECT MAX(sort_no) + 1 FROM dtb_class_name), 1), NOW(), NOW(), 'classname'
-WHERE JSON_VALUE(:className, '$.classNameId') REGEXP '^[0-9]+$'
+SELECT CAST(JSON_VALUE(CAST(:className AS CHAR), '$.classNameId') AS UNSIGNED), JSON_VALUE(CAST(:className AS CHAR), '$.name'), COALESCE((SELECT MAX(sort_no) + 1 FROM dtb_class_name), 1), NOW(), NOW(), 'classname'
+WHERE JSON_VALUE(CAST(:className AS CHAR), '$.classNameId') REGEXP '^[0-9]+$'
 ON DUPLICATE KEY UPDATE name=VALUES(name), update_date=NOW()
--- var/sql/tmail_template_update.sql
+++ var/sql/tmail_template_update.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(*) INTO @bemart_mail_template_found
 FROM dtb_mail_template
-WHERE id = CAST(JSON_VALUE(:entity, '$.mailTemplateId') AS UNSIGNED);
+WHERE id = CAST(JSON_VALUE(CAST(:entity AS CHAR), '$.mailTemplateId') AS UNSIGNED);
 UPDATE dtb_mail_template
-SET mail_subject = JSON_VALUE(:entity, '$.subject'), update_date = NOW()
-WHERE id = CAST(JSON_VALUE(:entity, '$.mailTemplateId') AS UNSIGNED);
+SET mail_subject = JSON_VALUE(CAST(:entity AS CHAR), '$.subject'), update_date = NOW()
+WHERE id = CAST(JSON_VALUE(CAST(:entity AS CHAR), '$.mailTemplateId') AS UNSIGNED);
 SELECT CASE WHEN @bemart_mail_template_found > 0 THEN 1 ELSE 0 END AS updated
