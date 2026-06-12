ab8b390becd6f063d002e2f2135fff2344cb31e1
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-12 18:02:22 +0900

    ページentrypointでAPP_CONTEXTを尊重する

--- bin/page.php
+++ bin/page.php
@@ -6,4 +6,6 @@ use MyVendor\BeMart\Bootstrap;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-exit((new Bootstrap())('cli-html-eccube-sql-hal-app', $GLOBALS, $_SERVER));
+$context = getenv('APP_CONTEXT') ?: 'cli-html-eccube-sql-hal-app';
+
+exit((new Bootstrap())($context, $GLOBALS, $_SERVER));
