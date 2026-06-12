4dbb494534a8f5947d1592e028bc0e8100607d81
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-06-12 23:27:34 +0900

    非会員購入のブラウザ遷移を直す

--- src/Resource/Page/Shopping/NonMember.php
+++ src/Resource/Page/Shopping/NonMember.php
@@ -6,6 +6,7 @@ namespace MyVendor\BeMart\Resource\Page\Shopping;
 
 use BEAR\ApiDoc\Annotation\Alps;
 use MyVendor\BeMart\Annotation\CsrfProtected;
+use MyVendor\BeMart\Auth\CartSessionPrefixInterface;
 use BEAR\Resource\Annotation\Link;
 use BEAR\Resource\Code;
 use BEAR\Resource\ResourceObject;
@@ -85,6 +86,7 @@ class NonMember extends ResourceObject
         private readonly BecomingInterface $becoming,
         private readonly CsrfToken $csrf,
         private readonly FormFactory $formFactory,
+        private readonly CartSessionPrefixInterface $cartSessionPrefix,
     ) {
     }
 
@@ -185,7 +187,7 @@ class NonMember extends ResourceObject
                 pref: $prefValue,
                 addr01: $values['addr01'],
                 addr02: $values['addr02'],
-                sessionPrefix: $sessionPrefix ?? 'session-prefix-1',
+                sessionPrefix: $this->cartSessionPrefix->prefix() ?? $sessionPrefix ?? 'session-prefix-1',
             ));
         } catch (SemanticVariableException $e) {
             [$field, $message] = self::semanticError($e);
